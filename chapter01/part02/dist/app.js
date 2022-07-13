1695f23f2fd5977d1a14794da96b3626fc8e8e7f
softira <1160390705@qq.com>
2022-07-13 08:06:22 +0800

    编译选项

---
+++ chapter01/part02/dist/app.js
@@ -0,0 +1,5 @@
+import { hi } from './test';
+console.log('hello');
+let b = 10;
+console.log(hi);
+export default void 0;
---
+++ chapter01/part02/src/app.ts
@@ -0,0 +1,15 @@
+// import {hi} from './test'
+console.log('hello');
+let b :number = 10
+// console.log(hi);
+
+function (a,b){
+
+}
+
+let box = document.querySelector('.box')
+box.addEventListener('click',()=>{})
+box?.addEventListener('click',()=>{})
+
+
+export default void 0
